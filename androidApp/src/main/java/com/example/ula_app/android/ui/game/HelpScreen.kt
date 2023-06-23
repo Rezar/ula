4f97f89ca9e09586342466375aead837eed3f1ef
Hao Wu <hhwu@ucdavis.edu>
2023-06-23 19:25:52 -0400

    presenting the FAQs as a card list instead of search box

--- androidApp/src/main/java/com/example/ula_app/android/ui/game/HelpScreen.kt
+++ androidApp/src/main/java/com/example/ula_app/android/ui/game/HelpScreen.kt
@@ -1,75 +1,215 @@
 package com.example.ula_app.android.ui.game
 
+import android.annotation.SuppressLint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.*
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+//import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ula_app.android.R
+import com.example.ula_app.android.ui.viewmodel.ExpandableCardModel
+import com.example.ula_app.android.ui.viewmodel.HelpViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 private const val TAG = "HelpScreen"
 
+private const val EXPAND_ANIMATION_DURATION: Int = 300
+private const val EXPANSTION_TRANSITION_DURATION: Int = 300
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun HelpScreen() {
+fun HelpScreen(
+    helpViewModel: HelpViewModel
+) {
 
-    // Obtain the questions from res
-    val allQuestions = stringArrayResource(R.array.FAQs).asList()
+    val cards by helpViewModel.cards.collectAsState()
+    val expandedCardIds by helpViewModel.expandedCardIdsList.collectAsState()
 
-    // This is the state of inputting text by user
-    val searchText = remember {
-        mutableStateOf("")
-    }
-    // This is a list of questions that filtered by the searchText
-    val questions = remember {
-        mutableStateOf(allQuestions)
-    }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        TextField(
-            value = searchText.value,
-            onValueChange = {
-                searchText.value = it
-
-                // Use filter to change the list of "questions" variable
-                questions.value = if (searchText.value.isBlank())
-                    allQuestions
-                else allQuestions.filter {
-                    it.contains(searchText.value, ignoreCase = true)
-                }
-            },
-            modifier = Modifier.fillMaxWidth(),
-            placeholder = { Text(text = "Search questions") },
-            singleLine = true
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        LazyColumn (
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f)
-        ) {
-            items(questions.value) {
-                Text(
-                    text = "$it",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 16.dp)
+    Scaffold {
+        LazyColumn {
+            items(cards, ExpandableCardModel::id) { card ->
+                ExpandableCard(
+                    card = card,
+                    onCardArrowClick = { helpViewModel.onCardArrowClicked(card.id) },
+                    expanded = expandedCardIds.contains(card.id),
                 )
             }
+        }
+    }
+}
+
+@SuppressLint("UnusedTransitionTargetStateParameter")
+@Composable
+fun ExpandableCard(
+    card: ExpandableCardModel,
+    onCardArrowClick: () -> Unit,
+    expanded: Boolean,
+) {
+    val transitionState = remember {
+        MutableTransitionState(expanded).apply {
+            targetState = !expanded
+        }
+    }
+    val transition = updateTransition(transitionState)
+    val cardBgColor by transition.animateColor({
+        tween(durationMillis = EXPAND_ANIMATION_DURATION)
+    }) {
+        if (expanded) Color(0xFFFFDA6D) else Color(0xFFFEFFFD)
+    }
+    val cardPaddingHorizontal by transition.animateDp({
+        tween(durationMillis = EXPAND_ANIMATION_DURATION)
+    }) {
+        if (expanded) 48.dp else 24.dp
+    }
+    val cardElevation by transition.animateDp({
+        tween(durationMillis = EXPAND_ANIMATION_DURATION)
+    }) {
+        if (expanded) 24.dp else 4.dp
+    }
+    val cardRoundedCorners by transition.animateDp({
+        tween(
+            durationMillis = EXPAND_ANIMATION_DURATION,
+            easing = FastOutSlowInEasing
+        )
+    }) {
+        if (expanded) 0.dp else 16.dp
+    }
+    val arrowRotationDegree by transition.animateFloat({
+        tween(durationMillis = EXPAND_ANIMATION_DURATION)
+    }) {
+        if (expanded) 0f else 180f
+    }
+
+    val context = LocalContext.current
+    val contentColour = remember {
+        Color(ContextCompat.getColor(context, R.color.gray_400))
+    }
+
+
+    Card(
+        backgroundColor = cardBgColor,
+        contentColor = contentColour,
+        elevation = cardElevation,
+        shape = RoundedCornerShape(cardRoundedCorners),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                horizontal = cardPaddingHorizontal,
+                vertical = 8.dp
+            )
+    ) {
+        Column {
+            Box {
+                CardArrow(
+                    degrees = arrowRotationDegree,
+                    onClick = onCardArrowClick
+                )
+                CardTitle(title = card.title)
+            }
+            ExpandableContent(visible = expanded)
+        }
+    }
+}
+
+@Composable
+fun CardArrow(
+    degrees: Float,
+    onClick: () -> Unit
+) {
+    IconButton(
+        onClick = onClick,
+        content = {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_helpcard),
+                contentDescription = "Expandable Arrow",
+                modifier = Modifier.rotate(degrees),
+            )
+        }
+    )
+}
+
+@Composable
+fun CardTitle(title: String) {
+    Text(
+        text = title,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        textAlign = TextAlign.Center,
+    )
+}
+
+@Composable
+fun ExpandableContent(
+    visible: Boolean = true,
+) {
+    val enterTransition = remember {
+        expandVertically(
+            expandFrom = Alignment.Top,
+            animationSpec = tween(EXPANSTION_TRANSITION_DURATION)
+        ) + fadeIn(
+            initialAlpha = 0.3f,
+            animationSpec = tween(EXPANSTION_TRANSITION_DURATION)
+        )
+    }
+    val exitTransition = remember {
+        shrinkVertically(
+            // Expand from the top.
+            shrinkTowards = Alignment.Top,
+            animationSpec = tween(EXPANSTION_TRANSITION_DURATION)
+        ) + fadeOut(
+            // Fade in with the initial alpha of 0.3f.
+            animationSpec = tween(EXPANSTION_TRANSITION_DURATION)
+        )
+    }
+
+    AnimatedVisibility(
+        visible = visible,
+        enter = enterTransition,
+        exit = exitTransition
+    ) {
+        Column(modifier = Modifier.padding(8.dp)) {
+            Spacer(modifier = Modifier.heightIn(100.dp))
+            Text(
+                text = "Expandable content here",
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
