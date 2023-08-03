38cc79a85455ac18473a3d4596251562ea3c9827
Hao Wu <hhwu@ucdavis.edu>
2023-08-02 23:54:13 -0400

    ios: add bottom navigation bar

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,16 +1,90 @@
 import SwiftUI
 import shared
 
-struct ContentView: View {
+struct Home: View {
 	let greet = Greeting().greet()
 
 	var body: some View {
 		Text(greet)
 	}
+    
 }
 
-struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-	}
+struct Stats: View {
+
+    var body: some View {
+        Text("Stats")
+    }
+    
 }
+
+struct Help: View {
+
+    var body: some View {
+        Text("Help")
+    }
+    
+}
+
+struct Setting: View {
+
+    var body: some View {
+        Text("Setting")
+    }
+    
+}
+
+struct Debug: View {
+
+    var body: some View {
+        Text("Debug")
+    }
+    
+}
+
+struct GameScreen: View {
+    
+    var body: some View {
+        TabView{
+            Home()
+                .tabItem{
+                    Text("Home")
+                    Image("ic_ula").resizable().frame(width: 32.0, height: 32.0).clipped()
+                }
+            
+            Stats()
+                .tabItem{
+                    Text("Stats")
+                    Image("ic_stats").resizable().frame(width: 32.0, height: 32.0).clipped()
+                }
+            
+            Help()
+                .tabItem{
+                    Text("Help")
+                    Image("ic_help").resizable().frame(width: 32.0, height: 32.0).clipped()
+                }
+            
+            Setting()
+                .tabItem{
+                    Text("Setting")
+                    Image("ic_cog").resizable().frame(width: 32.0, height: 32.0).clipped()
+                }
+            
+            Debug()
+                .tabItem{
+                    Text("Debug")
+                    Image("ic_debug").resizable().frame(width: 32.0, height: 32.0).clipped()
+                }
+        }
+    }
+    
+}
+
+
+
+
+//struct ContentView_Previews: PreviewProvider {
+//	static var previews: some View {
+//		GameScreen()
+//	}
+//}
